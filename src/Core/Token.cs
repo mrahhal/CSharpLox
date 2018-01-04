a907329ed4f8bfd31e5716261ec386767e4b00f6
Mohammad Rahhal <m.r992@hotmail.com>
2018-01-04 07:28:38 +0200

    + Token

---
+++ src/Core/Token.cs
@@ -0,0 +1,24 @@
+﻿namespace CSharpLox
+{
+	public class Token
+	{
+		public Token(TokenType type, string lexeme, object literal, int line)
+		{
+			Type = type;
+			Lexeme = lexeme;
+			Literal = literal;
+			Line = line;
+		}
+
+		public TokenType Type { get; }
+
+		public string Lexeme { get; }
+
+		public object Literal { get; }
+
+		public int Line { get; }
+
+		public override string ToString()
+			=> $"{Type} {Lexeme} {Literal}";
+	}
+}
