a655802de8b515d89d05eff56a210b8465ca109e
Mohammad Rahhal <m.r992@hotmail.com>
2018-01-05 21:36:49 +0200

    + Stmt

---
+++ src/Core/Stmt.cs
@@ -0,0 +1,181 @@
+﻿using System.Collections.Generic;
+
+namespace CSharpLox
+{
+	public abstract class Stmt
+	{
+		public interface IVisitor<R>
+		{
+			R VisitBlockStmt(Block stmt);
+
+			R VisitClassStmt(Class stmt);
+
+			R VisitExpressionStmt(Expression stmt);
+
+			R VisitFunctionStmt(Function stmt);
+
+			R VisitIfStmt(If stmt);
+
+			R VisitPrintStmt(Print stmt);
+
+			R VisitReturnStmt(Return stmt);
+
+			R VisitVarStmt(Var stmt);
+
+			R VisitWhileStmt(While stmt);
+		}
+
+		public class Block : Stmt
+		{
+			public Block(List<Stmt> statements)
+			{
+				Statements = statements;
+			}
+
+			public List<Stmt> Statements { get; }
+
+			private R Accept<R>(IVisitor<R> visitor)
+			{
+				return visitor.VisitBlockStmt(this);
+			}
+		}
+
+		public class Class : Stmt
+		{
+			public Class(Token name, Expr superclass, List<Stmt.Function> methods)
+			{
+				Name = name;
+				Superclass = superclass;
+				Methods = methods;
+			}
+
+			public Token Name { get; }
+			public Expr Superclass { get; }
+			public List<Stmt.Function> Methods { get; }
+
+			private R Accept<R>(IVisitor<R> visitor)
+			{
+				return visitor.VisitClassStmt(this);
+			}
+		}
+
+		public class Expression : Stmt
+		{
+			public Expression(Expr expression)
+			{
+				InnerExpression = expression;
+			}
+
+			public Expr InnerExpression { get; }
+
+			private R Accept<R>(IVisitor<R> visitor)
+			{
+				return visitor.VisitExpressionStmt(this);
+			}
+		}
+
+		public class Function : Stmt
+		{
+			public Function(Token name, List<Token> parameters, List<Stmt> body)
+			{
+				Name = name;
+				Parameters = parameters;
+				Body = body;
+			}
+
+			public Token Name { get; }
+			public List<Token> Parameters { get; }
+			public List<Stmt> Body { get; }
+
+			private R Accept<R>(IVisitor<R> visitor)
+			{
+				return visitor.VisitFunctionStmt(this);
+			}
+		}
+
+		public class If : Stmt
+		{
+			public If(Expr condition, Stmt thenBranch, Stmt elseBranch)
+			{
+				Condition = condition;
+				ThenBranch = thenBranch;
+				ElseBranch = elseBranch;
+			}
+
+			public Expr Condition { get; }
+			public Stmt ThenBranch { get; }
+			public Stmt ElseBranch { get; }
+
+			private R Accept<R>(IVisitor<R> visitor)
+			{
+				return visitor.VisitIfStmt(this);
+			}
+		}
+
+		public class Print : Stmt
+		{
+			public Print(Expr expression)
+			{
+				InnerExpression = expression;
+			}
+
+			public Expr InnerExpression { get; }
+
+			private R Accept<R>(IVisitor<R> visitor)
+			{
+				return visitor.VisitPrintStmt(this);
+			}
+		}
+
+		public class Return : Stmt
+		{
+			public Return(Token keyword, Expr value)
+			{
+				Keyword = keyword;
+				Value = value;
+			}
+
+			public Token Keyword { get; }
+			public Expr Value { get; }
+
+			private R Accept<R>(IVisitor<R> visitor)
+			{
+				return visitor.VisitReturnStmt(this);
+			}
+		}
+
+		public class Var : Stmt
+		{
+			public Var(Token name, Expr initializer)
+			{
+				Name = name;
+				Initializer = initializer;
+			}
+
+			public Token Name { get; }
+			public Expr Initializer { get; }
+
+			private R Accept<R>(IVisitor<R> visitor)
+			{
+				return visitor.VisitVarStmt(this);
+			}
+		}
+
+		public class While : Stmt
+		{
+			public While(Expr condition, Stmt body)
+			{
+				Condition = condition;
+				Body = body;
+			}
+
+			public Expr Condition { get; }
+			public Stmt Body { get; }
+
+			private R Accept<R>(IVisitor<R> visitor)
+			{
+				return visitor.VisitWhileStmt(this);
+			}
+		}
+	}
+}
