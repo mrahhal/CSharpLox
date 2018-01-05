41a29617d11fa5b9863f43ecffbd5abf9c420035
Mohammad Rahhal <m.r992@hotmail.com>
2018-01-05 22:58:53 +0200

    + EnvironmentScope

---
+++ src/Core/EnvironmentScope.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+namespace CSharpLox
+{
+	public class EnvironmentScope
+	{
+		private readonly Dictionary<string, object> _values = new Dictionary<string, object>();
+
+		public void Define(string name, object value)
+		{
+			_values[name] = value;
+		}
+
+		public object Get(Token name)
+		{
+			if (_values.TryGetValue(name.Lexeme, out var value))
+			{
+				return value;
+			}
+
+			throw new RuntimeError(name, "Undefined variable '" + name.Lexeme + "'.");
+		}
+	}
+}
